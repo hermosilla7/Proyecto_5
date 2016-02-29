7a7a6bbee2b45d03a09e03180f7d5921e1ceffad
unknown <9611.joan23@415-PORTC1194NC.joan23.local>
2016-02-29 12:23:49 +0100

    subida de estilo

--- index.php
+++ index.php
@@ -17,54 +17,96 @@
 
 <div class="content">
     <div class="destacado">
+        <div class="titlofrezco">
         <h1>Ofrezco</h1>
         <?php
             if (isset($_SESSION['id'])){
         ?>
         <a href="ofrezco_necesito_insert.php"><img src="img/plus.png" /></a> 
+         </div>
         <?php
             }
 
             while ($ofrezco = mysqli_fetch_array($result_actividades)){
                 $fichero = "img/$ofrezco[img]";
                 if (file_exists($fichero) && (($ofrezco['img']) != '')) {
-                    echo "<img src='$fichero' width='60%' /><br />";
-                } else {
-                    echo "<img src ='img/no_disponible.jpg'width='40%' /><br />";
-                }
-                echo "Título actividad: ".utf8_encode($ofrezco['nombre'])."<br />";
-                echo "Fecha: ".$ofrezco['fecha']."<br />";
-                echo "Tipo de actividad: ".utf8_encode($ofrezco['ntact'])."<br />";
-                echo "Dirección: ".utf8_encode($ofrezco['direccion'])."<br />";
-                echo "Monedas: ".$ofrezco['compensacion']."<br />";
+                    echo "<div class='notic'>";
+                    echo "<h1>".utf8_encode($ofrezco['nombre'])."</h1><br />";
+                     echo"<div class='notcont'>";
+                echo "<h2>Tipo de actividad: </h2><br />".utf8_encode($ofrezco['ntact']).".<br /><br />";
+                echo "<h2>Fecha: </h2><br />".$ofrezco['fecha'].".<br /><br />";
+               
+                echo "<h2>Dirección: </h2><br />".utf8_encode($ofrezco['direccion']).".<br /><br />";
+                echo "<h2>Monedas: ".$ofrezco['compensacion']."</h2><br /><br />";
                 echo "<a href='#'>Ver más</a><br /><br />";
+                echo "</div>";
+                    echo"<div class='notimg'>";
+                    echo "<img src='$fichero' width='60%' /><br />";
+                     echo"</div>";
+                } else {
+                    echo "<div class='notic'>";
+                    echo  "<h1>".utf8_encode($ofrezco['nombre'])."</h1><br />";
+                     echo"<div class='notcont'>";
+                
+                   echo "<h2>Tipo de actividad: </h2><br />".utf8_encode($ofrezco['ntact']).".<br /><br />";
+                echo "<h2>Fecha: </h2><br />".$ofrezco['fecha'].".<br /><br />";
+               
+                echo "<h2>Dirección: </h2><br />".utf8_encode($ofrezco['direccion']).".<br /><br />";
+                echo "<h2>Monedas: ".$ofrezco['compensacion']."</h2><br /><br />";
+                echo "<a href='#'>Ver más</a><br /><br />";
+                echo "</div>";
+                     echo"<div class='notimg'>";
+                    echo "<img src ='img/no_disponible.jpg'width='40%' /><br />";
+                     echo"</div>";
+                }
+                echo "</div>";
             }
         ?>
     </div>
   
 
     <div class="necesito">
+        <div class="titlofrezco">
         <h1>Necesito</h1>
         <?php
             if (isset($_SESSION['id'])){
         ?>
         <a href="ofrezco_necesito_insert.php"><img src="img/plus.png" /></a>  
+    </div>
         <?php
             }
 
             while ($necesito = mysqli_fetch_array($result_actividades2)){
                 $fichero = "img/$necesito[img]";
                 if (file_exists($fichero) && (($necesito['img']) != '')) {
-                    echo "<img src='$fichero' width='60%' /><br />";
-                } else {
-                    echo "<img src ='img/no_disponible.jpg'width='40%' /><br />";
-                }
-                echo "Título actividad: ".utf8_encode($necesito['nombre'])."<br />";
-                echo "Fecha: ".$necesito['fecha']."<br />";
-                echo "Tipo de actividad: ".utf8_encode($necesito['ntact'])."<br />";
-                echo "Dirección: ".utf8_encode($necesito['direccion'])."<br />";
-                echo "Monedas: ".$necesito['compensacion']."<br />";
+                    echo "<div class='notic'>";
+                    echo "<h1>".utf8_encode($necesito['nombre'])."</h1><br />";
+                     echo"<div class='notcont'>";
+                echo "<h2>Tipo de actividad: </h2><br />".utf8_encode($necesito['ntact']).".<br /><br />";
+                echo "<h2>Fecha: </h2><br />".$necesito['fecha'].".<br /><br />";
+               
+                echo "<h2>Dirección: </h2><br />".utf8_encode($necesito['direccion']).".<br /><br />";
+                echo "<h2>Monedas: ".$necesito['compensacion']."</h2><br /><br />";
                 echo "<a href='#'>Ver más</a><br /><br />";
+                echo "</div>";
+                    echo"<div class='notimg'>";
+                    echo "<img src='$fichero' width='60%' /><br />";
+                     echo"</div>";
+                } else {
+                    echo "<div class='notic'>";
+                    echo "<h1>".utf8_encode($necesito['nombre'])."</h1><br />";
+                    echo"<div class='notcont'>";
+                    echo "<h2>Tipo de actividad: </h2><br />".utf8_encode($necesito['ntact']).".<br /><br />";
+                    echo "<h2>Fecha: </h2><br />".$necesito['fecha'].".<br /><br />";
+                    echo "<h2>Dirección: </h2><br />".utf8_encode($necesito['direccion']).".<br /><br />";
+                    echo "<h2>Monedas: ".$necesito['compensacion']."</h2><br /><br />";
+                    echo "<a href='#'>Ver más</a><br /><br />";
+                    echo "</div>";
+                    echo"<div class='notimg'>";
+                    echo "<img src ='img/no_disponible.jpg'width='40%' /><br />";
+                    echo"</div>";
+                }
+                echo "</div>";
             }
         ?>
     </div>
