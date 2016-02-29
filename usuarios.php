a91672a1f6ab8053695b79506107909989a38483
unknown <9611.joan23@415-PORTC1194NC.joan23.local>
2016-02-29 11:02:39 +0100

    usuarios

--- usuarios.php
+++ usuarios.php
@@ -3,13 +3,16 @@
     $consulta_usuarios = "SELECT * FROM usuario where activo = 1";
     $result_usuarios = mysqli_query($con, $consulta_usuarios);
 ?>
-    <div id="usuarios">
+<div class="titluse">
+        <h1>Usuarios</h1>
+    </div>
+    <div id="usuarios" class="usuarios">
         <div class="prin-img" style="margin-bottom: 15px;">
 
         <?php
             $usuariosArray = [];
             while ($usuario = mysqli_fetch_array($result_usuarios)) {
-                echo "<b style='margin-top: 15px;'>Nombre:</b> ";
+                echo "<div class='users'><b style='margin-top: 15px;'>Nombre:</b> ";
                 echo utf8_encode($usuario['nombre']);
                 echo "<br/>";
                 echo "<b>Apellidos:</b> ";
@@ -17,11 +20,12 @@
                 echo "<br/>";
                 echo "<b>Correo:</b> ";
                 echo utf8_encode($usuario['correo']);
+                 echo "<br/>";
         ?>
             <a href="usuarios_modificar_admin.php?id=<?php echo $usuario['id']; ?>">Editar usuario</a>
             <a href="usuarios_baja_admin.proc.php?id=<?php echo $usuario['id']; ?>">Eliminar usuario</a>
         <?php 
-                echo "<br/><br/>";
+                echo "<br/></div><br/>";
             }
         ?>
             <a href="#" class="crunchify-top"><img src ="img/btt.png" style="float: right;" width="50px" height="50px"></a>
