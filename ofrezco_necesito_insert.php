9410746fc5232ef14b7f5bf54242c07a7430a2c6
unknown <9611.joan23@415-PORTC1194NC.joan23.local>
2016-03-01 12:25:37 +0100

    subida de inserts

--- ofrezco_necesito_insert.php
+++ ofrezco_necesito_insert.php
@@ -30,17 +30,33 @@
 			return enviar;
 		}
 	</script>
-
+   <div class="titleact">
+        <h1>Creación de evento</h1>
+    </div>
 	<div class="mod-form">
-		<h1>Registro</h1>
+		
  		<div class="form-group ">
 			<form name="f1" action="ofrezco_necesito_insert.proc.php" method="post" enctype="multipart/form-data" onsubmit="return validar();">
-				<br></br><br></br>
+				<h3>Ofrezco</h3>
+				<input id="radio" type="radio" name="peticion" <?php if( $user['nivel'] == '0' ) { ?>checked="checked"<?php } ?> value='0' >
+				<h3>Necesito</h3>		
+				<input id="radio" type="radio" name="peticion" <?php if( $user['nivel'] == '1' ) { ?>checked="checked"<?php } ?> value='1' >
+				
+				<br>
+								<!-- TIPO ACTIVIDAD -->
+				<select id="selects" name="id_tipo_actividad">
+					<option value="">Seleccionar actividad</option>
+					<?php
+					while($fila=mysqli_fetch_array($result_actividad)){
+						echo utf8_encode("<option value=\"$fila[id]\">$fila[nombre]</option>");
+					}
+		        	?>
+			    </select></br><br>
 				<div class="form-group">
 					<input type="text" name="nombre" class="form-control" placeholder="Nombre" required><br>
 				</div>
 				<div class="form-group">
-					<input type="text" name="descripcion" class="form-control" placeholder="Descripción" required><br>
+					<textarea name="descripcion" class="form-control" placeholder="Descripción" rows="5" required></textarea><br>
 				</div>
 				<div class="form-group">
 					<input type="datetime-local" name="fecha" class="form-control" placeholder="Fecha/hora" required><br>
@@ -57,20 +73,10 @@
 				<div class="form-group">
 					<input type="number" name="compensacion" class="form-control" placeholder="Compensación" required><br>
 				</div>
-				<input id="radio" type="radio" name="peticion" <?php if( $user['nivel'] == '0' ) { ?>checked="checked"<?php } ?> value='0' >Ofrezco</br><br>		
-				<input id="radio" type="radio" name="peticion" <?php if( $user['nivel'] == '1' ) { ?>checked="checked"<?php } ?> value='1' >Necesito</br><br>
 				<div class="form-group">
 					<input type="file" name="foto" id="foto" class="form-control"></br>
 				</div>
-							<!-- TIPO ACTIVIDAD -->
-				<select id="selects" name="id_tipo_actividad">
-					<option value="">Seleccionar actividad</option>
-					<?php
-					while($fila=mysqli_fetch_array($result_actividad)){
-						echo utf8_encode("<option value=\"$fila[id]\">$fila[nombre]</option>");
-					}
-		        	?>
-			    </select></br><br>
+			
 				<button type="submit" class="log-btn" onClick="validar()" name="acce">Registrar</button>
 				<button type="button" class="sign-btn" onClick="window.location.href='index.php'">Volver</button>
 			</form>
