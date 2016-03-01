48aeb8863d2005c99594c13db8ddf17e6fa8261f
Gluque1678 <92076.joan23@fje.edu>
2016-03-01 08:46:16 +0100

    donaciones

--- donaciones.php
+++ donaciones.php
@@ -62,23 +62,26 @@ Introduce el código de seguridad*: img
 
 <br/>
  <div class="footer">
- 		<p><Suscríbete haz donativos a LA AS0CIACIÓN DE JUBILADOS MÉS DE 65 con Paypal
+ 		<p> Colaborar con un nuestro CLUB SOLIDARIO MÉS DE 65 es una buena forma para ayudar a los demas.
+			Elegir nuestro CLUB SOLIDARIO MÉS DE 65 es muy buena elección. Nuestra labor,
+			es colaborar y ayudar a las personas que lo necesitan y que éstas también aporten ayuda a otras.
+		Todas las personas tenemos alguna cualidad que otras no poseen, por eso creemos que todos nos podemos
+		ayudar.
 
-PayPal. La forma rápida y segura de pagar en Internet. 
-Campaña ASOCIACIÓN DE JUBILADOS MÉS DE 65 2016
- 
-¿Cómo puedo colaborar con la ASOCIACIÓN DE JUBILADOS MÉS DE 65?
+		Tú también puedes colaborar y ayudar hacer posible los proyectos de desarrollo que realiza nuestro 
+		CLUB SOLIDARIO MÉS DE 65.
+		Cada euro es destinado para ayudar a sus asociados a realizar y disfrutar de las actividades.
 
-Colaborar con una ASOCIACIÓN DE JUBILADOS MÉS DE 65 es una buena forma para ayudar a los demas.
-Elegir la ASOCICIÓN DE JUBILADOS MÉS DE 65 es muy buena elección. Nuestra labor,
-esfuerzo y colaboración constante en ayudar a las personas más necesitadas.
-
-Tú también puedes colaborar para ayudar hacer posible los proyectos de desarrollo que realiza nuestra ASOCIACIÓN DE JUBILADOS MÉS DE 65.
-Cada euro es destinado para ayudar a sus asociados a realizar y disfrutar de las actividades.
+ 		¿Cómo puedo colaborar con nuestro CLUB SOLDARIO MÉS DE 65?
 
 
-¡Deja de buscar una ASOCIACIÓN para colaborar, porque ya la has encontrado! ¡Anímate a colaborar con la ASOCIACIÓN DE JUBILADOS MÉS DE 65
-para así ayudar a lograr un mundo mejor para todos.</p>
+ 		Suscríbete y haz un donativo a nuestro CLUB SOLIDARIO MÉS DE 65.
+ 		La forma rápida y segura de pagar en Internet con PAYPAL. 
+
+
+	 ¡Anímate a colaborar con nuestro CLUB SOLIDARIO MÉS DE 65
+	para así ayudar a lograr un mundo mejor para nuestros asociados.</p>
+ 		
  		<center>
 		<div class="formaPago">
        	 <img src="./img/servired.jpg" width="85" height="50">
