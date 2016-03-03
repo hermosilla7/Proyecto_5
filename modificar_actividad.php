88f564461864b96daf9131d0956afd4785673c18
PerezRaul <7400.joan23@fje.edu>
2016-03-04 00:48:51 +0100

    Modificación actividad a medias

--- modificar_actividad.php
+++ modificar_actividad.php
@@ -1,8 +1,9 @@
 <?php
     include 'header.php';
 
-    $consulta_actividades = "SELECT actividad.*, tipo_actividad.* FROM actividad INNER JOIN tipo_actividad ON actividad.id_usuario = usuario.id WHERE actividad.id = $_REQUEST[actividad]";
+    $consulta_actividades = "SELECT actividad.nombre, actividad.descripcion, actividad.fecha, , tipo_actividad.* FROM actividad INNER JOIN tipo_actividad ON actividad.id_tipo_actividad = tipo_actividad.id WHERE actividad.id = $_REQUEST[actividad]";
     $result_actividades = mysqli_query($con, $consulta_actividades);
+    $act = mysqli_fetch_array($result_actividades);
 ?>
 
 	<div class="titleact">
@@ -11,24 +12,42 @@
 	
 	
 	<div class="form-group ">
-		<form name="f1" action="modificar_actividad.proc.php" method="post" enctype="multipart/form-data"><br /></br />
-			<div class="mod-form3">
+		<form name="f1" action="ofrezco_necesito_insert.proc.php" method="post" enctype="multipart/form-data" onsubmit="return validar();">
 				<div class="form-group">
-					<input type="text" name="nombre" class="form-control" placeholder="Nombre" required <?php if(isset($nom_user)){ echo "value = '".$nom_user."'"; } ?>><br />
+					<input type="text" name="nombre" class="form-control" placeholder="Nombre" required value="<?php echo utf8_encode($act['nombre']);  ?>" ><br>
 				</div>
 				<div class="form-group">
-					<input type="text" name="apellidos" class="form-control" placeholder="Apellidos" required <?php if(isset($apellidos)){ echo "value = '".$apellidos."'"; } ?>><br />
+					<textarea name="descripcion" class="form-control" placeholder="Descripción" required ><?php echo utf8_encode($act['descripcion']);  ?></textarea><br>
 				</div>
 				<div class="form-group">
-					<input type="text" name="correo" class="form-control" placeholder="Correo" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" title="Formato en minúsculas. Ejemplo: info@gmail.com" required <?php if(isset($mail)){ echo "value = '".$mail."'"; } ?>><br />
+					<input type="datetime-local" name="fecha" class="form-control" placeholder="Fecha/hora" required value="<?php echo utf8_encode($act['fecha']);  ?>" ><br>
 				</div>
 				<div class="form-group">
-					<input type="text" name="telefono" class="form-control" placeholder="Teléfono" pattern="[0-9]{9}" title="Formato correcto: 618589666" required <?php if(isset($telf)){ echo "value = '".$telf."'"; } ?>><br />
+					<input type="text" name="direccion" class="form-control" placeholder="Dirección" value="<?php echo utf8_encode($act['direccion']);  ?>" ><br>
 				</div>
+				<div class="form-group">
+					<input type="text" name="ubicacion_lat" class="form-control" placeholder="Latitud" value="<?php echo utf8_encode($act['ubicacion_lat']);  ?>" ><br>
+				</div>
+				<div class="form-group">
+					<input type="text" name="ubicacion_lon" class="form-control" placeholder="Longitud" value="<?php echo utf8_encode($act['ubicacion_lon']);  ?>"><br>
+				</div>
+				<input id="radio" type="radio" name="peticion" <?php if( $user['nivel'] == '0' ) { ?>checked="checked"<?php } ?> value='0' >Ofrezco</br><br>
+				<input id="radio" type="radio" name="peticion" <?php if( $user['nivel'] == '1' ) { ?>checked="checked"<?php } ?> value='1' >Necesito</br><br>
+				<div class="form-group">
+					<input type="file" name="foto" id="foto" class="form-control"></br>
+				</div>
+							<!-- TIPO ACTIVIDAD -->
+				<select id="selects" name="id_tipo_actividad">
+					<option value="">Seleccionar actividad</option>
+					<?php
+					while($fila=mysqli_fetch_array($result_actividades)){
+						echo utf8_encode("<option value=\"$fila[id]\">$fila[nombre]</option>");
+					}
+		        	?>
+			    </select></br><br>
 				<button type="submit" class="log-btn" onClick="validar()" name="acce">Registrar</button>
 				<button type="button" class="sign-btn" onClick="window.location.href='index.php'">Volver</button>
-			</div>
-		</form>
+			</form>
 	</div>
 
 
